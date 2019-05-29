871d4c61a50ce6276ca0b0a6a26433a381888da8
Robin Hood <fordarnold@gmail.com>
2019-05-29 13:59:22 +0200

    Resource: User
    Configured eloquent API resources ~ https://scotch.io/tutorials/laravel-eloquent-api-resources

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,16 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'username' => $this->username,
+            'email' => $this->email,
+
+            // include linked categories
+            // 'categories' => new CategoryResourceCollection($this->categories),
+        ];
     }
 }
--- app/Http/Resources/UserResourceCollection.php
+++ app/Http/Resources/UserResourceCollection.php
@@ -14,6 +14,13 @@ class UserResourceCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+
+        return [
+            'data' => UserResource::collection($this->collection),
+            'meta' => [
+                'total_count' => $this->collection->count()
+            ],
+        ];
     }
 }
