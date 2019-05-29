d690b654d816cfeeb355c909423e7a6c42c248a3
Robin Hood <fordarnold@gmail.com>
2019-05-29 17:49:00 +0200

    Entity Relationships: Admin
    Added polymorphic relationship setup for the Admin model ~ https://logrocket.com/blog/polymorphic-relationships-in-laravel

--- app/Admin.php
+++ app/Admin.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Admin extends Model
 {
-    //
+
+    /**
+     * Polymorphic Relationship: an Admin account must be linked to a single User
+     *
+     * @see https://laravel.com/docs/5.8/eloquent-relationships#polymorphic-relationships
+     */
+    public function user() {
+    	
+        return $this->morphOne(User::class, 'userable');
+    }
 }
